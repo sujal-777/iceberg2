80ce5562af060ea524c898c1cfe2f216289ce9a1
Sujal <palsujal707@gmail.com>
2025-04-10 00:34:10 +0530

    vercel

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import { Star } from "lucide-react";
-import { Autour_One } from "next/font/google";
 import Discover from "@/components/AboutUs/Discover";
 import Founder from "@/components/AboutUs/Founder";
 import Mission from "@/components/AboutUs/Mission";
--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { boolean } from "drizzle-orm/gel-core";
 import IcyChat from "./chatbot";
 import Image from "next/image";
 import Link from "next/link";
