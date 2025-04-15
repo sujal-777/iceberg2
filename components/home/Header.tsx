6effd19bdbcbd1690e1193aec6968c3f4184ac0e
Sujal <palsujal707@gmail.com>
2025-04-15 21:18:51 +0530

    deploy error resolve

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,7 +1,6 @@
 "use client"
 import IcyChat from "./chatbot";
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
