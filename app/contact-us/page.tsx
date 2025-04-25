0b508af9b54a460fa7521d92c347b349befb6e5f
aavi4012-cmd <heyitzavinash@gmail.com>
2025-04-25 10:45:44 +0530

    add

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,32 +1,17 @@
-// import ContactUs from "@/components/ContactPage/Contactus";
-// import FAQSection from "@/components/home/FaqSec";
-// import FreeBatchesSection from "@/components/ContactPage/instant"
-// import Contact from "@/components/home/Contact-Us"
-
-// export default function PAGE() {
-//   return (
-//     <>
-//       <ContactUs />
-//       <FAQSection />
-//       <FreeBatchesSection/>1
-//       <Contact/>
-//     </>
-//   );
-// }
-
-
 import ContactUs from "@/components/ContactPage/Contactus";
 import FAQSection from "@/components/home/FaqSec";
-import FreeBatchesSection from "@/components/ContactPage/instant";
-import Contact from "@/components/home/Contact-Us";
+import FreeBatchesSection from "@/components/ContactPage/instant"
+import Contact from "@/components/home/Contact-Us"
 
 export default function PAGE() {
   return (
-    <div className="flex flex-col gap-12 px-4 md:px-8 lg:px-16 py-8 max-w-screen-xl mx-auto">
+    <>
       <ContactUs />
       <FAQSection />
-      <FreeBatchesSection />
-      <Contact />
-    </div>
+      <FreeBatchesSection/>1
+      <Contact/>
+    </>
   );
 }
+
+
