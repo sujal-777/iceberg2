3b275dea2a28ab4207d6b08b7d7652341a61356a
mkiran <152247979+M6839@users.noreply.github.com>
2025-05-24 17:59:37 +0530

    descriptive question page added

--- app/test-series/free-mock-test/ca-mock/[examId]/[testSeriesId]/attempt/Profile/page.tsx
+++ app/test-series/free-mock-test/ca-mock/[examId]/[testSeriesId]/attempt/Profile/page.tsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
+import { MdPhone } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+
 const Profile = () => {
   return (
    <div className='flex justify-center'>
-       <form className='flex flex-col gap-[35px] my-[40px] w-[515px] h-[523px] px-[26px] py-[41px] border-[1px] border-black rounded-[10px]'>
+       <form  className='flex flex-col gap-[35px] my-[40px] w-[515px] h-[523px] px-[26px] py-[41px] border-[1px] border-black rounded-[10px]'>
         <h1 className='text-[25px] font-bold text-center'>Complete Your Profile</h1>
-        <label className='flex flex-col gap-[6px] text-[16px] font-[500]'>
+        <label className='relative flex flex-col gap-[6px] text-[16px] font-[500]'>
           Full Name
-          <input type='text' placeholder='Enter Your Full Name' className='h-[45px] font-normal rounded-[10px] border-[1px] border-black'></input>
+          <input type='text' placeholder='Enter Your Full Name' className='px-[40px] h-[45px] font-normal rounded-[10px] border-[1px] border-black' required></input>
+          <FaUser className='absolute top-[45px] left-[10px] text-[20px] text-[#8281A5]'/>
         </label>
-        <label className='flex flex-col gap-[6px] text-[16px] font-[500]'>
+        <label className='relative flex flex-col gap-[6px] text-[16px] font-[500]'>
           Email Address
-          <input type='mail' placeholder='Enter your Email Address' className='h-[45px] font-normal rounded-[10px] border-[1px] border-black'></input>
+          <input type='mail' placeholder='Enter your Email Address' className='px-[40px] h-[45px] font-normal rounded-[10px] border-[1px] border-black' required></input>
+          <MdEmail className='absolute top-[45px] left-[10px] text-[20px] text-[#8281A5]'/>
         </label>
-        <label className='flex flex-col gap-[6px] text-[16px] font-[500]'>
+        <label className='relative flex flex-col gap-[6px] text-[16px] font-[500]'>
           Mobile Number
-          <input type='text' placeholder='Enter your Mobile Number' className='h-[45px] font-normal rounded-[10px] border-[1px] border-black'></input>
+          <input type='text' placeholder='Enter your Mobile Number' className='px-[40px] h-[45px] font-normal rounded-[10px] border-[1px] border-black' required></input>
+          <MdPhone className='absolute top-[45px] left-[10px] text-[20px] text-[#8281A5]'/>
         </label>
         <Link href={'/test-series/free-mock-test/ca-mock/682b7e3f506c24cdafbdeda8/682b801b506c24cdafbdedad/attempt/Submission'}>
-        <button className='text-[16px] text-white bg-[#0048B0] rounded-[10px] font-[500] w-full h-[45px]'>Continue</button>
+        <button type="submit" className='text-[16px] text-white bg-[#0048B0] rounded-[10px] font-[500] w-full h-[45px]' >Continue</button>
         </Link>
       
        </form>
--- app/test-series/free-mock-test/ca-mock/[examId]/[testSeriesId]/attempt/page.jsx
+++ app/test-series/free-mock-test/ca-mock/[examId]/[testSeriesId]/attempt/page.jsx
@@ -2,13 +2,13 @@
 "use client"
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
-
+import { useRef } from 'react';
 
 export default function MockTestPage() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [answers, setAnswers] = useState({});
   const [reviewList, setReviewList] = useState([]);
-  const [typeQuest,setTypeQuest]=useState(false)
+  const [typeQuest,setTypeQuest]=useState('MCQ');
    const [questionsData,setQuestionsData] = useState([]);
   const [loading, setLoading] = useState(true);
   const currentQuestion = questionsData[currentIndex];
@@ -17,7 +17,24 @@ export default function MockTestPage() {
   const [secondsLeft, setSecondsLeft] = useState(THREE_HOURS_IN_SECONDS);
 const [visited, setVisited] = useState([]);
 
+const inputRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
+  const handleFiles = (files) => {
+    const fileList = Array.from(files);
+    console.log('Selected files:', fileList);
+    setSelectedFiles(fileList);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      handleFiles(e.dataTransfer.files);
+      e.dataTransfer.clearData();
+    }
+  };
 useEffect(() => {
   const currentQId = questionsData[currentIndex]?._id;
   if (currentQId && !visited.includes(currentQId)) {
@@ -94,8 +111,9 @@ const progress=((currentIndex + 1) / questionsData.length) * 100
        
   {loading ? (<p className="text-gray-500">Loading questions…</p>) : questionsData.length === 0 ? (<p className="text-gray-500">No questions found for this test.</p>) : 
     (<div className='flex flex-col'>
-       <p className='ml-auto text-[20px] font-bold'>{formatTime(secondsLeft)}</p>
-      <div className="relative flex justify-center gap-[10px] ">
+        <p className='ml-auto mr-[40px] text-[20px] font-bold'>{formatTime(secondsLeft)}</p>
+        {/*MCQ questions*/}
+      {typeQuest==='MCQ' &&( <div className="relative flex justify-center gap-[10px] ">
         <div className="bg-white w-[966px] p-6 rounded-[10px] shadow border-[1px] border-black">
            <div className='flex justify-between '>
            <p className='text-[18px] font-[500] mb-[10px]'>Questions <span>{currentIndex+1}</span> of <span>{questionsData.length}</span></p>
@@ -157,8 +175,8 @@ const progress=((currentIndex + 1) / questionsData.length) * 100
       {/* Sidebar with Question Numbers */}
       <div className='relative max-w-[526px] p-[19px] border-[1px] border-black rounded-[10px]'>
             <div className='flex'>
-              <button className={`border-[1px] border-black rounded-l-[10px] h-[40px] w-[236px] ${typeQuest===false ? "bg-[#0048B0] text-white" :"bg-white text-black"}`}>MCQ questions</button>
-               <button className={`border-[1px] border-black rounded-r-[10px] h-[40px] w-[236px]  ${typeQuest===true ? "bg-[#0048B0] text-white" :"bg-white text-black"}`} >Descriptive questions</button>
+              <button className={`border-[1px] border-black rounded-l-[10px] h-[40px] w-[236px] bg-[#0048B0] text-white`}>MCQ questions</button>
+               <button className={`border-[1px] border-black rounded-r-[10px] h-[40px] w-[236px] `} onClick={()=>setTypeQuest('Descriptive')}>Descriptive questions</button>
             </div>
             <div className='grid grid-cols-6 gap-[16px] mt-[20px] '>
                       {questionsData.map((q, idx) => {
@@ -191,7 +209,60 @@ const progress=((currentIndex + 1) / questionsData.length) * 100
             <a href='/test-series/free-mock-test/ca-mock/682b7e3f506c24cdafbdeda8/682b801b506c24cdafbdedad/attempt/Profile' className=''><button className={`absolute top-[500px] border-[1px] border-[#0048B0] text-[#0048B0] rounded-[10px] h-[40px] w-[470px] hover:bg-[#0048B0] hover:text-white `} >Submit Examination</button></a>
         </div>
         
-       </div>
+       </div>) }
+
+
+
+               {/*descriptive questions*/}
+
+      {typeQuest==='Descriptive' &&( <div className="relative flex justify-center h-[843px] gap-[10px] ">
+        <div className="bg-white w-[966px] p-6 rounded-[10px] shadow border-[1px] border-black">
+          <div className='h-[655px] w-[527px] bg-blue-500 mx-auto'></div>
+          <div className='flex justify-between items-center mt-[80px]'>
+              <div className='flex items-center gap-[18px]'>
+                 <button className="px-4 py-2 rounded">
+              Mark for Review
+            </button>
+                <p>Clear responce</p>
+            </div>
+            <div className='flex gap-[10px]'>
+              <button className='h-[45px] w-[128px] text-[#0048B0] rounded-[12px] border-[1px] border-[#0048B0]'>Previous</button>
+              <button className='h-[45px] w-[128px] bg-[#0048B0] text-white rounded-[12px] border-[1px] border-[#0048B0]'>Save & Next</button>
+            </div>
+            </div>
+        </div>
+        <div className='relative w-[526px] p-[19px] border-[1px] border-black rounded-[10px]'>
+           <div className='flex'>
+              <button className={`border-[1px] border-black rounded-l-[10px] h-[40px] w-[236px] `} onClick={()=>setTypeQuest('MCQ')} >MCQ questions</button>
+               <button className={`border-[1px] border-black rounded-r-[10px] h-[40px] w-[236px]  bg-[#0048B0] text-white`}>Descriptive questions</button>
+            </div>
+            <div className='flex  items-center gap-[20px] px-[29px] bg-[#E3EEFF] mt-[40px] h-[102px] rounded-[15px] border-[1px] border-[#0048B0]'>
+            <img src='/pdfformat.png' width={48} height={65} alt=''/>
+            <p className='flex flex-col  text-[20px] font-[600] text-[#0048B0]'>theprojetks-design-tokens.pdf<span className=' text-[16px] font-normal text-black'>10.0MB</span></p>
+            </div>
+            <h1 className='text-[20px] font-bold mt-[150px] mb-[20px]'>Submit your answere PDF here</h1>
+            <div className='flex flex-col justify-center items-center gap-[18px] h-[205px] w-[472px] border-dashed border-[1px] border-[#000000] rounded-[10px]' onDragOver={(e) => {
+        e.preventDefault();
+        setIsDragging(true);
+      }}onDragLeave={() => setIsDragging(false)} onDrop={handleDrop} onClick={() => inputRef.current.click()}>
+              <input ref={inputRef} type="file" multiple className="hidden" onChange={(e) => handleFiles(e.target.files)}/>
+              <h1 className='text-[17px] font-[600]'>Drag & Drop your file here</h1>
+              {selectedFiles.length > 0 && <ul className="mt-4 text-left text-sm text-gray-700">
+          {selectedFiles.map((file, index) => (
+            <li key={index}>• {file.name}</li>
+          ))}
+        </ul>}
+              <div className='flex items-center gap-[10px] justify-center'>
+                <div className='w-[180px] border-[0.5px] border-[gray]'></div>
+                or
+                <div className='w-[180px] border-[0.5px] border-[gray]'></div>
+              </div>
+              <div className='bg-[#0048B0] rounded-[12px] text-white w-[140px] h-[45px] flex items-center justify-center courser-pointer' >Select File<input type='file' className='hidden'></input></div>
+              <p className='text-[13px]'>Upload your file here</p>
+            </div>
+            <a href='/test-series/free-mock-test/ca-mock/682b7e3f506c24cdafbdeda8/682b801b506c24cdafbdedad/attempt/Profile' className=''><button className={`mt-[150px] border-[1px] border-[#0048B0] text-[#0048B0] rounded-[10px] h-[40px] w-[470px] hover:bg-[#0048B0] hover:text-white `} >Submit Examination</button></a>
+        </div>
+       </div>)}
        </div>
        )}
        
